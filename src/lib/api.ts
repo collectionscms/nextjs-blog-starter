a0a6f0f249f26e31e482dac2eead64f995e97edc
kazaneshimizu <ks@rocketa.co.jp>
2024-08-25 14:56:13 +0900

    fix: rename

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,24 +1,23 @@
 import { Post } from "@/interfaces/post";
 
-const endpoint = process.env.COLLECTIONS_API_ENDPOINT;
-const key = process.env.COLLECTIONS_API_KEY;
+const apiUrl = process.env.COLLECTIONS_API_URL;
+const apiKey = process.env.COLLECTIONS_API_KEY;
 
 export async function getPostBySlug(slug: string): Promise<Post> {
-
-  const res = await fetch(`${endpoint}/api/v1/posts/${slug}`, {
+  const res = await fetch(`${apiUrl}/api/v1/posts/${slug}`, {
     headers: {
-      "Authorization": `Bearer ${key}`,
-    }
+      Authorization: `Bearer ${apiKey}`,
+    },
   });
   const data = await res.json();
   return data.post;
 }
 
 export async function getAllPosts(): Promise<Post[]> {
-  const res = await fetch(`${endpoint}/api/v1/posts`, {
+  const res = await fetch(`${apiUrl}/api/v1/posts`, {
     headers: {
-      "Authorization": `Bearer ${key}`,
-    }
+      Authorization: `Bearer ${apiKey}`,
+    },
   });
   const data = await res.json();
   return data.posts;
