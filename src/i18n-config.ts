899cb41c2908b6cb769b4b6f449a2bc3f420482c
kazaneshimizu <ks@rocketa.co.jp>
2024-08-25 14:27:20 +0900

    chore: reset locales

--- src/i18n-config.ts
+++ src/i18n-config.ts
@@ -1,6 +1,6 @@
 export const i18n = {
-  defaultLocale: "en",
-  locales: ["en", "cs", "ja"],
+  defaultLocale: "en-us",
+  locales: ["en-us", "es", "ja", "pt-pt", "pt-br", "zh"],
 } as const;
 
 export type Locale = (typeof i18n)["locales"][number];
