82d3f4009f35b35a30288b555a38db85f994ed4c
kazaneshimizu <ks@rocketa.co.jp>
2024-08-27 06:17:25 +0900

    refactor: add revalidate

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,8 +3,12 @@ import { Post } from "@/interfaces/post";
 const apiUrl = process.env.COLLECTIONS_API_URL;
 const apiKey = process.env.COLLECTIONS_API_KEY;
 
-export async function getPostBySlug(slug: string): Promise<Post> {
+export async function getPostBySlug(
+  slug: string,
+  revalidate: number
+): Promise<Post> {
   const res = await fetch(`${apiUrl}/api/v1/posts/${slug}`, {
+    next: { revalidate },
     headers: {
       Authorization: `Bearer ${apiKey}`,
     },
@@ -13,8 +17,9 @@ export async function getPostBySlug(slug: string): Promise<Post> {
   return data.post;
 }
 
-export async function getAllPosts(): Promise<Post[]> {
+export async function getAllPosts(revalidate: number): Promise<Post[]> {
   const res = await fetch(`${apiUrl}/api/v1/posts`, {
+    next: { revalidate },
     headers: {
       Authorization: `Bearer ${apiKey}`,
     },
